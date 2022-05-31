350fef4bd6c689219874381986fcf0b587bd34a0
Deepak <djdeepakjoshi6@gmail.com>
2022-05-31 16:52:14 +0530

    succesfull deployment

---
+++ precache-manifest.0590704037dc1ef40a22bea9727670d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370dafeb83c85f4bcb2edd38751392e0",
+    "url": "/finalproject-VI-SEM-deepakjoshi-prathamverma/index.html"
+  },
+  {
+    "revision": "f1557814adc3f8595abb",
+    "url": "/finalproject-VI-SEM-deepakjoshi-prathamverma/static/css/main.872f18c8.chunk.css"
+  },
+  {
+    "revision": "b9c5a21ba2fc7f86b7ff",
+    "url": "/finalproject-VI-SEM-deepakjoshi-prathamverma/static/js/2.8b8cdf51.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/finalproject-VI-SEM-deepakjoshi-prathamverma/static/js/2.8b8cdf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1557814adc3f8595abb",
+    "url": "/finalproject-VI-SEM-deepakjoshi-prathamverma/static/js/main.ba56a231.chunk.js"
+  },
+  {
+    "revision": "b8b93252f1ff74ce1c34",
+    "url": "/finalproject-VI-SEM-deepakjoshi-prathamverma/static/js/runtime-main.0899bb76.js"
+  }
+]);
